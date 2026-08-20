6afb361bc9aa91138bb9bdc78c32e41a1c3c05d5
Kathleen Pang <kathleenpangqiyu@gmail.com>
2026-08-20 17:56:58 +0800

    Attempt Responsive Design

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// Wait for the HTML elements to load before running the code
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
+function showSidebar(){
+    const sidebar = document.querySelector('.side-bar')
+    sidebar.style.display='flex'
+}
 
-    // Toggle the active classes when the hamburger button is clicked
-    menuToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('mobile-active');
-        menuToggle.classList.toggle('open');
-    });
-});
+function hideSidebar(){
+    const sidebar = document.querySelector('.side-bar')
+    sidebar.style.display='none'
+}
